b2ea86e534965bbcda4669b330835e91cf4e3946
KitKatFarrelly <49258630+KitKatFarrelly@users.noreply.github.com>
2022-10-16 02:18:16 -0400

    blank tkinter gui

--- Main-GUI.py
+++ Main-GUI.py
@@ -1,6 +1,16 @@
 import os
 import sys
 import tkinter as tk
-from tkinter import ttk
 import serial
 from serial.tools import list_ports
+
+class MainGui(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("LIDAR GUI")
+        self.geometry("1200x720")
+
+
+if __name__ == "__main__":
+    app = MainGui()
+    app.mainloop()
